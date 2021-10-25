9b56fe6dd1738c7adfc44904146313edce5b274a
lakshmi <lakshmireddy1625@gmail.com>
2021-10-25 12:49:08 +0530

    added git demo to local repository

---
+++ src/main/java/com/example/demo/hello/HelloWorldGitDemo.java
@@ -0,0 +1,14 @@
+package com.example.demo.hello;
+
+public class HelloWorldGitDemo {
+	
+	public void m1() {
+		System.out.println("Hello world m1 method");
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello world main method");
+		HelloWorldGitDemo h=new HelloWorldGitDemo();
+		h.m1();
+	}
+
+}
