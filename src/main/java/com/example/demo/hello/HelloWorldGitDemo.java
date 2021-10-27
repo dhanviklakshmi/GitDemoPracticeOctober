8fe25cd66507a6c5f96ad4b94e0b07e5adca1cba
lakshmi <lakshmireddy1625@gmail.com>
2021-10-27 16:35:35 +0530

    This is for checking 3rd time

--- src/main/java/com/example/demo/hello/HelloWorldGitDemo.java
+++ src/main/java/com/example/demo/hello/HelloWorldGitDemo.java
@@ -5,6 +5,8 @@ public class HelloWorldGitDemo {
 	public void m1() {
 		System.out.println("Hello world m1 method");
 		System.out.println("Hello world2 m1 method");
+                System.out.println("Hello world2 m1 method edited for checking");
+
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello world main method");
